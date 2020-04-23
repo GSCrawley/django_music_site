fd9f4090477bb29a1cf9d982ac2b95f2e4c73fb5
GSCrawley <gscrawley@gmail.com>
2020-04-23 09:40:18 -0700

    updating

--- music/urls.py
+++ music/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns =[
-    path('', BandListView.as_view(), name='home'),
+    path('home/', BandListView.as_view(), name='home'),
     path('artist/<int:artist_id>/', views.artist_detail, name='artist_detail'),
     path('album/<int:album_id>/album', views.album_detail, name='album_detail'),
     path('song/<int:song_id>/song', views.song_detail, name='song_detail')
